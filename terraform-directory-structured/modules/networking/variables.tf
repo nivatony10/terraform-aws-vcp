6d81ca374c10cc7ab8de6dbf981b0dcab348e9d9
nivatony10 <84965181+nivatony10@users.noreply.github.com>
2022-04-04 20:53:13 +0200

    update to match settings

--- terraform-directory-structured/modules/networking/variables.tf
+++ terraform-directory-structured/modules/networking/variables.tf
@@ -1,30 +1,50 @@
 variable "project" {
-  description = "The name of the project"
+  description = "EKS-terraform"
 }
 
 variable "environment" {
   description = "The deployment environment"
+  default     = "production"
 }
 
 variable "region" {
-  description = "The AWS Region"
+  description = "ap-south-1"
 }
 
 variable "availability_zones" {
   type        = list(any)
-  description = "The names of the availability zones to use"
+  description = "ap-south-1a"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block of the vpc"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets_cidr" {
   type        = list(any)
-  description = "The CIDR block for the public subnet"
+  default = ["10.0.10.0/24", "10.0.20.0/24"]
+
 }
 
 variable "private_subnets_cidr" {
   type        = list(any)
-  description = "The CIDR block for the private subnet"
+  default = ["10.0.30.0/24", "10.0.40.0/24"]
+
+}
+
+}
+
+variable "public2_subnets_cidr" {
+  type        = list(any)
+  default = ["10.0.21.0/24", "10.0.31.0/24"]
+
+}
+
+}
+
+variable "private2_subnets_cidr" {
+  type        = list(any)
+  default = ["10.0.41.0/24", "10.0.51.0/24"]
+
 }
