dd786ddc85b58d71462e0256f651e5b8ff616d9c
nivatony10 <84965181+nivatony10@users.noreply.github.com>
2022-04-04 22:44:26 +0200

    Update main.tf

--- terraform-directory-structured/main.tf
+++ terraform-directory-structured/main.tf
@@ -22,7 +22,6 @@ module "vcp" {
   name               = var.name
   region             = var.region
   private_subnet_ids = module.vpc.private_subnet_ids
-  public_subnet_ids  = module.vpc.public_subnet_ids
   instance_type      = "t3a.large"
   public_key_name    = "deploykey"
   filename           = "${path.module}/cluster.yaml"
