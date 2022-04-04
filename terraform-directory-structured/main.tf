a49ebd9a9589bc36a610157ee8fd2797cef3391f
nivatony10 <84965181+nivatony10@users.noreply.github.com>
2022-04-04 22:36:12 +0200

    update main.tf modules cluster

--- terraform-directory-structured/main.tf
+++ terraform-directory-structured/main.tf
@@ -15,3 +15,15 @@ module "vcp" {
   public1_subnets_cidr  = var.public1_subnets_cidr
   private1_subnets_cidr = var.private1_subnets_cidr
 }
+
+  
+  module "config" {
+  source             = "./"
+  name               = var.name
+  region             = var.region
+  private_subnet_ids = module.vpc.private_subnet_ids
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  instance_type      = "t3.large"
+  public_key_name    = "deploykey"
+  filename           = "${path.module}/cluster.yaml"
+}
